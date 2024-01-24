c6f492952239be7fbd30420e7d534043dabca38b
mbtechy <156566460+mbtechy@users.noreply.github.com>
2024-01-24 06:40:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -760,7 +760,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("🦋 I couldn't find any movie in that name. Pls enter correct movie name and retry once more or else wait for admin to upload the same in the database 🦋")
+        k = await msg.reply("HELLO {first} ❌ NOT FOUND IN OUR DB ❌ \n\n🦋CHECK SPELLING OR WAIT TILL {ADMINS} UPLOADS IT🦋")
         await asyncio.sleep(8)
         await k.delete()
         return
